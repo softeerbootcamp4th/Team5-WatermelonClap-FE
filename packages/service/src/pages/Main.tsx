2289b10bfdaceec5126138f311b3a2096420dd0c
thgee <thgeee@naver.com>
2024-07-30 13:39:47 +0900

    fix: theme import 코드 삭제
    - eslint때문에 빌드 터짐 이슈 해결

--- packages/service/src/pages/Main.tsx
+++ packages/service/src/pages/Main.tsx
@@ -10,7 +10,6 @@ import { DEMO_PAGE_ROUTE } from "@service/constants/routes";
 import AssetLoaderOverlay from "@service/common/components/AssetLoaderOverlay/Overlay";
 import AssetWrapper from "@service/common/components/AssetLoaderOverlay/AssetWrapper";
 import { BorderBeam } from "@service/common/components/BorderBeam";
-import { theme } from "@watermelon-clap/core";
 
 export const Main = () => {
   const scrollTargetRef = useRef<HTMLDivElement>(null);
