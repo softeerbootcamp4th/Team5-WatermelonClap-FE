4866d93af81c828de599be85d7b2c12b0dd6fe6b
DaeWon9 <qkreodnjs97@naver.com>
2024-07-30 20:44:27 +0900

    chore: 이름 변경 AppHeader -> GlobalNavigatioBar

--- packages/service/src/common/components/GlobalNavigationBar/GlobalNavigationBar.tsx
+++ packages/service/src/common/components/GlobalNavigationBar/GlobalNavigationBar.tsx
@@ -1,15 +1,15 @@
 import { headerContainerStyles, logoStyles } from "./GlobalNavigationBar.css";
-import { ReactComponent as LogoLong } from "@service/assets/logo-long.svg";
-import { AppHeaderNavs } from "./AppHeaderNavs";
+import { ReactComponent as LogoLong } from "public/images/gnb/logo-long.svg";
+import GlobalNavs from "./GlobalNavs";
 
-const AppHeader = () => {
+const GlobalNavigationBar = () => {
   return (
     <header css={headerContainerStyles}>
       <LogoLong css={logoStyles} />
 
-      <AppHeaderNavs />
+      <GlobalNavs />
     </header>
   );
 };
 
-export default AppHeader;
+export default GlobalNavigationBar;
