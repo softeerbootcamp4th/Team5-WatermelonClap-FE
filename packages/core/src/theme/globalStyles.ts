d71b693866768a4a793981348bc042f7e291b4a5
thgee <thgeee@naver.com>
2024-07-31 10:35:47 +0900

    chore: 정적파일 public으로 옮기기 및 불필요한 파일 삭제

--- packages/core/src/theme/globalStyles.ts
+++ packages/core/src/theme/globalStyles.ts
@@ -3,37 +3,32 @@ import { css } from "@emotion/react";
 const globalStyles = css`
   @font-face {
     font-family: "Pretendard-Bold";
-    src: url("src/assets/fonts/pretendard/Pretendard-Bold.woff2")
-      format("woff2");
+    src: url("/fonts/pretendard/Pretendard-Bold.woff2") format("woff2");
   }
   @font-face {
     font-family: "Pretendard-Medium";
-    src: url("src/assets/fonts/pretendard/Pretendard-Medium.woff2")
-      format("woff2");
+    src: url("/fonts/pretendard/Pretendard-Medium.woff2") format("woff2");
   }
   @font-face {
     font-family: "Pretendard-SemiBold";
-    src: url("src/assets/fonts/pretendard/Pretendard-SemiBold.woff2")
-      format("woff2");
+    src: url("/fonts/pretendard/Pretendard-SemiBold.woff2") format("woff2");
   }
   @font-face {
     font-family: "PyeongChang-Bold";
-    src: url("src/assets/fonts/pyeongChang/PyeongChang-Bold.otf")
-      format("opentype");
+    src: url("/fonts/pyeongChang/PyeongChang-Bold.otf") format("opentype");
   }
   @font-face {
     font-family: "PyeongChang-Regular";
-    src: url("src/assets/fonts/pyeongChang/PyeongChang-Regular.otf")
-      format("opentype");
+    src: url("/fonts/pyeongChang/PyeongChang-Regular.otf") format("opentype");
   }
   @font-face {
     font-family: "PyeongChangPeace-Bold";
-    src: url("src/assets/fonts/pyeongChangPeace/PyeongChangPeace-Bold.otf")
+    src: url("/fonts/pyeongChangPeace/PyeongChangPeace-Bold.otf")
       format("opentype");
   }
   @font-face {
     font-family: "PyeongChangPeace-Light";
-    src: url("src/assets/fonts/pyeongChangPeace/PyeongChangPeace-Light.otf")
+    src: url("/fonts/pyeongChangPeace/PyeongChangPeace-Light.otf")
       format("opentype");
   }
 `;
