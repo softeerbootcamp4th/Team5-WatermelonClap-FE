e9f5a897c0bd02847b95d9b707b4eb133596819b
DaeWon9 <qkreodnjs97@naver.com>
2024-07-31 16:51:26 +0900

    feat: Footer 컴포넌트 구현

--- packages/service/src/App.tsx
+++ packages/service/src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { GlobalNavigationBar } from "./common/components/GlobalNavigationBar";
+import { Footer } from "./common/components/Footer";
 
 export const App = () => {
   return (
     <div>
       <GlobalNavigationBar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
