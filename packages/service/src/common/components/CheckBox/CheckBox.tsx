dc3f7253df234c06597eca51abcd376cb9d291dd
DaeWon9 <qkreodnjs97@naver.com>
2024-08-01 18:39:12 +0900

    feat: 체크박스의 스타일을 커스터마이징 가능하게 변경

--- packages/service/src/common/components/CheckBox/CheckBox.tsx
+++ packages/service/src/common/components/CheckBox/CheckBox.tsx
@@ -1,77 +1,93 @@
+import { ReactNode } from "react";
+import { SerializedStyles } from "@emotion/react";
 import { motion } from "framer-motion";
 import {
   checkBoxContainerStyle,
   checkBoxButtonStyle,
   checkBoxLabelStyle,
-  checkBoxStyle,
   checkBoxSvgStyle,
+  checkBoxStyle,
 } from "./CheckBox.css";
 
-interface CheckBoxProps {
+interface CheckBoxProps extends React.InputHTMLAttributes<HTMLInputElement> {
   isChecked: boolean;
   setIsChecked: (isChecked: boolean) => void;
   text: string;
+  interpolation?: {
+    labelStyle?: SerializedStyles;
+    checkBoxStyle?: (isChecked: boolean) => SerializedStyles;
+    svgStyle?: SerializedStyles;
+  };
+  svgIcon?: ReactNode;
 }
 
 const tickVariants = {
   checked: {
     pathLength: 1,
     opacity: 1,
-    transition: {
-      duration: 0.2,
-      delay: 0.2,
-    },
+    transition: { duration: 0.2, delay: 0.2 },
   },
   unchecked: {
     pathLength: 0,
     opacity: 0,
-    transition: {
-      duration: 0.2,
-    },
+    transition: { duration: 0.2 },
   },
 };
 
 /**
- * CheckBox의 checked 상태는 외부에서 받아온 상태를 사용한다.
- * Label에 해당하는 부분은 string만 들어오는 것으로 설정
+ * CheckBox 컴포넌트는 외부에서 받은 상태를 사용하여 체크 여부를 제어합니다.
+ * text는 체크박스의 레이블 텍스트입니다 string을 받습니다.
+ * interpolation을 통해 체크박스의 스타일을 보간할 수 있습니다.
+ * svgIcon을 통해 사용자 정의 SVG 아이콘을 제공할 수 있습니다.
  */
-export const CheckBox = ({ isChecked, setIsChecked, text }: CheckBoxProps) => {
+export const CheckBox = ({
+  isChecked,
+  setIsChecked,
+  text,
+  interpolation = {},
+  svgIcon,
+  ...props
+}: CheckBoxProps) => {
   return (
-    <div css={checkBoxContainerStyle}>
+    <div css={checkBoxContainerStyle} {...props}>
       <button
         css={checkBoxButtonStyle}
         onClick={() => setIsChecked(!isChecked)}
       >
-        <div css={checkBoxStyle(isChecked)}>
-          <motion.svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="3.5"
-            stroke="currentColor"
-            css={checkBoxSvgStyle}
-            initial={false}
-            animate={isChecked ? "checked" : "unchecked"}
-          >
-            <motion.path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4.5 12.75l6 6 9-13.5"
-              variants={tickVariants}
-            />
-          </motion.svg>
+        <div
+          css={[
+            checkBoxStyle(isChecked),
+            interpolation.checkBoxStyle?.(isChecked),
+          ]}
+        >
+          {svgIcon && isChecked ? (
+            svgIcon
+          ) : (
+            <motion.svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="3.5"
+              stroke="currentColor"
+              css={[checkBoxSvgStyle, interpolation.svgStyle]}
+              initial={false}
+              animate={isChecked ? "checked" : "unchecked"}
+            >
+              <motion.path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 12.75l6 6 9-13.5"
+                variants={tickVariants}
+              />
+            </motion.svg>
+          )}
         </div>
       </button>
       <motion.label
-        css={checkBoxLabelStyle}
-        animate={{
-          x: isChecked ? [0, -4, 0] : [0, 4, 0],
-        }}
+        css={[checkBoxLabelStyle, interpolation.labelStyle]}
+        animate={{ x: isChecked ? [0, -4, 0] : [0, 4, 0] }}
         initial={false}
-        transition={{
-          duration: 0.3,
-          ease: "easeOut",
-        }}
+        transition={{ duration: 0.3, ease: "easeOut" }}
       >
         {text}
       </motion.label>
