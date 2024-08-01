399ea9f797b2cb5d0cf84e83ed5f7b70ef5f4514
thgee <thgeee@naver.com>
2024-08-01 13:24:30 +0900

    feat: 메인페이지 타이머 애니메이션

--- packages/service/src/components/main/EventPeriod/EventPeriod.tsx
+++ packages/service/src/components/main/EventPeriod/EventPeriod.tsx
@@ -1,8 +1,12 @@
 import * as style from "./style";
-import { theme } from "@watermelon-clap/core";
+import { baseStyles, theme } from "@watermelon-clap/core";
 import { css } from "@emotion/react";
+import { Timer } from "./Timer";
 
 export const EventPeriod = () => {
+  const today = new Date(); // Get the current date
+  const futureDate = new Date(today); // Create a new date object
+  futureDate.setDate(today.getDate() + 2);
   return (
     <div css={style.container}>
       <h1
@@ -14,18 +18,8 @@ export const EventPeriod = () => {
         아반떼 N 출시 기념 이벤트
       </h1>
       <h2 css={style.period}>2024. 07. 29 ~ 08. 18</h2>
-      {/* 남은 날짜, 시간 계산하여 표시하기*/}
-      <h2 css={style.timer}>
-        <span
-          css={css`
-            color: ${theme.color.eventBlue};
-            margin-right: 80px;
-          `}
-        >
-          07일
-        </span>
-        12 : 58 : 32
-      </h2>
+      <div css={baseStyles.margin.mg48}></div>
+      <Timer date={futureDate} />
     </div>
   );
 };
