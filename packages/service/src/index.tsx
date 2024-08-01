7da88936a0cf6c7f1283788394973fae0dfd38dc
DaeWon9 <qkreodnjs97@naver.com>
2024-08-01 16:46:18 +0900

    feat: Modal Proivder 구현

--- packages/service/src/index.tsx
+++ packages/service/src/index.tsx
@@ -5,10 +5,15 @@ import { globalStyles } from "@watermelon-clap/core";
 import { Global } from "@emotion/react";
 import { router } from "./router.tsx";
 import { RouterProvider } from "react-router-dom";
+import { ModalProvider } from "./common/providers/ModalProvider.tsx";
+import { ModalContainer } from "./common/components/ModalContainer";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Global styles={globalStyles} />
-    <RouterProvider router={router} />
+    <ModalProvider>
+      <Global styles={globalStyles} />
+      <RouterProvider router={router} />
+      <ModalContainer />
+    </ModalProvider>
   </React.StrictMode>,
 );
