cc6fdba3145ee1c7f9629f35c0241ed3a8976487
thgee <thgeee@naver.com>
2024-08-03 15:15:16 +0900

    chore: class 선택자 -> emotion style로 변경

--- packages/service/src/components/main/EventCard/style.ts
+++ packages/service/src/components/main/EventCard/style.ts
@@ -10,23 +10,23 @@ export const eventCard = (isMainEvent: boolean) => css`
   white-space: pre-wrap;
   gap: 38px;
   box-shadow: 0px 0px 100px 0px rgba(255, 255, 255, 0.5);
+`;
 
-  .title {
-    ${theme.font.pyeongChangPeace.bold.title};
-    ${baseStyles.margin.mg12}
-    font-size: 40px;
-  }
+export const eventNumber = css`
+  border-radius: 50%;
+  background-color: ${theme.color.black};
+  width: 44px;
+  height: 44px;
+  color: ${theme.color.white};
+  margin: 0 auto;
+  ${baseStyles.flex.center}
+  ${theme.font.pretend.bold.body24}
+`;
 
-  .event-number {
-    border-radius: 50%;
-    background-color: ${theme.color.black};
-    width: 44px;
-    height: 44px;
-    color: ${theme.color.white};
-    margin: 0 auto;
-    ${baseStyles.flex.center}
-    ${theme.font.pretend.bold.body24}
-  }
+export const eventTitle = css`
+  ${theme.font.pyeongChangPeace.bold.title};
+  ${baseStyles.margin.mg12}
+  font-size: 40px;
 `;
 
 export const joinButton = css`
