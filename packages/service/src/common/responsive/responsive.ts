e36b1ce175a0c927ed8a0ff516e2213b869645d7
thgee <thgeee@naver.com>
2024-08-05 03:26:02 +0900

    chore: 반응형 breakpoint constant 폴더에 분리

--- packages/service/src/common/responsive/responsive.ts
+++ packages/service/src/common/responsive/responsive.ts
@@ -1,14 +1,12 @@
 import { css, SerializedStyles } from "@emotion/react";
-
-// Galaxy S20 Ultra 기준
-export const MOBILE_MAX_WIDTH = "412px" as const;
+import { MOBILE_BREAKPOINT } from "@service/constants/breakpoints";
 
 /**
  * 두 번째 인자로 원하는 픽셀값을 넣어줄 수 있음, default 값은 "412px"
  */
 export const mobile = (
   styles: SerializedStyles,
-  maxWidth: string = MOBILE_MAX_WIDTH,
+  maxWidth: string = MOBILE_BREAKPOINT,
 ) => css`
   @media screen and (max-width: ${maxWidth}) {
     ${styles}
