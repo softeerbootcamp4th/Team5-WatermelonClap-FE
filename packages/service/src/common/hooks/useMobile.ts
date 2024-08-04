ca6f3e5e6053d713f592ffd624b81abfb0cf19d6
thgee <thgeee@naver.com>
2024-08-05 03:26:02 +0900

    chore: 반응형 breakpoint constant 폴더에 분리

--- packages/service/src/common/hooks/useMobile.ts
+++ packages/service/src/common/hooks/useMobile.ts
@@ -1,12 +1,12 @@
+import { MOBILE_BREAKPOINT } from "@service/constants/breakpoints";
 import { useMediaQuery } from "react-responsive";
-import { MOBILE_MAX_WIDTH } from "../responsive/responsive";
 
 /**
  * 모바일 환경이면 true, 아니면 false 반환
  *
  * 인자로 원하는 픽셀값을 넣어줄 수 있음, default 값은 "412px"
  */
-export const useMobile = (maxWidth: string = MOBILE_MAX_WIDTH) => {
+export const useMobile = (maxWidth: string = MOBILE_BREAKPOINT) => {
   const isMobile = useMediaQuery({
     query: `(max-width: ${maxWidth})`,
   });
---
+++ packages/service/src/constants/breakpoints.ts
@@ -0,0 +1,3 @@
+// Galaxy S20 Ultra 기준
+export const MOBILE_BREAKPOINT = "412px" as const;
+export const GNB_BREAKPOINT = "800px" as const;
