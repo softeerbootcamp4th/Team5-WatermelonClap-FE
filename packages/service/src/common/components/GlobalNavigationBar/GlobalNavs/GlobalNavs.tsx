72d44dc85968fe88309cf02de7e34586b0f6cb89
thgee <thgeee@naver.com>
2024-08-05 02:45:26 +0900

    feat: gnb 메뉴버튼 생성

---
+++ packages/service/src/common/components/GlobalNavigationBar/GlobalNavs/GlobalNavs.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { ReactComponent as NLogo } from "public/images/gnb/n-logo.svg";
+import { linkStyles, navsContainerStyles, nLogoStyles } from "./GlobalNavs.css";
+
+const GlobalNavs = () => {
+  return (
+    <div css={navsContainerStyles}>
+      <Link to="#" css={linkStyles}>
+        아반떼 N
+      </Link>
+      <Link to="#" css={linkStyles}>
+        내 아반떼 N 뽑기
+      </Link>
+      <Link to="#" css={linkStyles}>
+        <NLogo css={nLogoStyles} /> 퀴즈
+      </Link>
+      <Link to="#" css={linkStyles}>
+        내 컬렉션
+      </Link>
+      <Link to="#" css={linkStyles}>
+        응모 내역 확인
+      </Link>
+    </div>
+  );
+};
+
+export default GlobalNavs;
