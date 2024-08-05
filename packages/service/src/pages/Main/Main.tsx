e8e4f73a4e5583b20cc308382544ce49677857c4
thgee <thgeee@naver.com>
2024-08-05 17:51:54 +0900

    feat: 메인페이지 레이아웃 배치

--- packages/service/src/pages/Main/Main.tsx
+++ packages/service/src/pages/Main/Main.tsx
@@ -2,14 +2,20 @@ import { EventCard } from "@service/components/main/EventCard";
 import * as style from "./Main.css";
 import { Banner, EventPeriod, Expectations } from "@service/components/main";
 import { eventData } from "./eventData";
+import { Space } from "@service/common/styles/Space";
+import { useMobile } from "@service/common/hooks/useMobile";
 
 export const Main = () => {
+  const isMobile = useMobile();
+
   return (
     <>
       <Banner />
       <div css={style.mainBg}>
         <EventPeriod />
+        <Space size={isMobile ? 30 : 100} />
         <Expectations />
+        <Space size={isMobile ? 30 : 120} />
         <div css={style.eventCardWrap}>
           {eventData.map((data) => (
             <EventCard eventData={data} key={data.id} />
