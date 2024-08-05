563727a5574578acb6a2c08b02cb83b9396ee003
thgee <thgeee@naver.com>
2024-08-05 10:47:49 +0900

    chore: 메인페이지 배경 crop 및 위치조절

--- packages/service/src/pages/Main/Main.tsx
+++ packages/service/src/pages/Main/Main.tsx
@@ -6,16 +6,17 @@ import { theme } from "@watermelon-clap/core";
 
 export const Main = () => {
   return (
-    <div>
-      <img src="images/main/main-bg.svg" css={style.mainBg} />
+    <>
       <Banner />
-      <EventPeriod />
-      <Expectations />
-      <div css={[theme.flex.center, theme.gap.gap24]}>
-        {eventData.map((data) => (
-          <EventCard eventData={data} key={data.id} />
-        ))}
+      <div css={style.mainBg}>
+        <EventPeriod />
+        <Expectations />
+        <div css={[theme.flex.center, theme.gap.gap24]}>
+          {eventData.map((data) => (
+            <EventCard eventData={data} key={data.id} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
