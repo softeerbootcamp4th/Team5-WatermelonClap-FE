6b88424f9f87808e8401ff307e3c8f249bbffc6f
thgee <thgeee@naver.com>
2024-08-05 15:12:58 +0900

    feat: EventCard 컴포넌트 반응형 구현

--- packages/service/src/components/main/EventCard/EventCard.tsx
+++ packages/service/src/components/main/EventCard/EventCard.tsx
@@ -3,32 +3,26 @@ import * as style from "./EventCard.css";
 import { IEventData } from "@service/pages/Main/eventData";
 import { Button, ButtonVariant } from "@service/common/components/Button";
 import { css } from "@emotion/react";
+import { useMobile } from "@service/common/hooks/useMobile";
 
 interface IEventDataProps {
   eventData: IEventData;
 }
 export const EventCard = ({ eventData }: IEventDataProps) => {
+  const isMobile = useMobile();
+
   return (
     <div css={style.eventCard(eventData.id === 1)}>
       <div css={style.eventNumber}>{eventData.id}</div>
       <div css={[theme.flex.column, theme.gap.gap32]}>
         <div>
-          <h2
-            css={css`
-              ${theme.font.preB28};
-              margin-top: 28px;
-              margin-bottom: 12px;
-            `}
-          >
-            {eventData.eventType}
-          </h2>
+          <h2 css={style.eventType}>{eventData.eventType}</h2>
           <h1 css={style.eventTitle}>{eventData.title}</h1>
-          <h3 css={theme.font.preB24}> {eventData.subTitle}</h3>
+          <h3 css={style.subTitle}> {eventData.subTitle}</h3>
         </div>
+        <img src={eventData.img} width={isMobile ? 200 : 300} />
 
-        <img src={eventData.img} />
-
-        <p css={[theme.font.preM20, theme.margin.mg32]}>{eventData.desc}</p>
+        <p css={style.desc}>{eventData.desc}</p>
 
         <Button variant={ButtonVariant.LONG} css={style.joinButton}>
           참여하기
