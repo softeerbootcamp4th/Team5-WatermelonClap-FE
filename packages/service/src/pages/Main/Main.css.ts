457195667a2782e7bbdd505d02fac4c4efa74fe2
thgee <thgeee@naver.com>
2024-08-05 17:16:26 +0900

    merge: resolve conflict

--- packages/service/src/pages/Main/Main.css.ts
+++ packages/service/src/pages/Main/Main.css.ts
@@ -3,4 +3,12 @@ import { css } from "@emotion/react";
 export const mainBg = css`
   background-image: url("images/main/main-bg.svg");
   background-size: cover;
+  padding-bottom: 200px;
+`;
+
+export const eventCardWrap = css`
+  display: flex;
+  gap: 24px;
+  justify-content: center;
+  flex-wrap: wrap;
 `;
