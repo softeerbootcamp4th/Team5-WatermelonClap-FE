a184a2aa952081f24b48b8c4e7418bdcb90d9e9f
thgee <thgeee@naver.com>
2024-08-08 18:02:48 +0900

    feat: [PickEvnet 페이지] 이벤트 약관

--- packages/service/src/pages/PickEvent/PickEvent.css.ts
+++ packages/service/src/pages/PickEvent/PickEvent.css.ts
@@ -1,8 +1,36 @@
 import { css } from "@emotion/react";
+import { mobile } from "@service/common/responsive/responsive";
+import { theme } from "@watermelon-clap/core";
 
 export const bg = css`
   background-image: url("images/common/main-bg.svg");
   background-size: cover;
   padding-top: 54px;
   padding-bottom: 200px;
+`;
+
+export const termTitleStyle = css`
+  color: ${theme.color.gray400};
+  ${theme.font.preM20};
+  line-height: 140%;
+  align-self: self-start;
+  padding-top: 94px;
+
+  ${mobile(css`
+    font-size: 10px;
+    padding-top: 47px;
+  `)}
+`;
+
+export const termListStyle = css`
+  color: ${theme.color.gray400};
+  ${theme.font.preM20};
+  line-height: 140%;
+  align-self: self-start;
+  margin-left: 2rem;
+
+  ${mobile(css`
+    font-size: 10px;
+    margin-left: 1rem;
+  `)}
 `;
--- packages/service/src/pages/PickEvent/PickEvent.tsx
+++ packages/service/src/pages/PickEvent/PickEvent.tsx
@@ -1,12 +1,20 @@
 import { JoinInfo } from "@service/components/pickEvent/JoinInfo/JoinInfo";
 import * as style from "./PickEvent.css";
 import { CardCarousel } from "@service/components/pickEvent/CardCarousel";
+import { pickEventTerms, pickEventTermsTitle } from "@service/constants/terms";
 
 export const PickEvent = () => {
   return (
     <div css={style.bg}>
       <JoinInfo />
       <CardCarousel />
+
+      <span css={style.termTitleStyle}>{pickEventTermsTitle}</span>
+      <ul css={style.termListStyle}>
+        {pickEventTerms.map((term) => (
+          <li>{term}</li>
+        ))}
+      </ul>
     </div>
   );
 };
