7570fc8f06e06938febfa33f1980316ea712a6d1
thgee <thgeee@naver.com>
2024-08-12 17:56:11 +0900

    feat: [PartsCollection 페이지]  배경, 타이틀 구현

--- packages/service/src/pages/PartsCollection/PartsCollection.css.ts
+++ packages/service/src/pages/PartsCollection/PartsCollection.css.ts
@@ -1,1 +1,16 @@
 import { css } from "@emotion/react";
+import { theme } from "@watermelon-clap/core";
+
+export const mainBg = css`
+  background-image: url("images/common/main-bg.svg");
+  background-size: cover;
+  padding-bottom: 200px;
+`;
+
+export const pageTitle = css`
+  text-align: center;
+  ${theme.font.pcpB}
+  font-size : calc(50px + 2vw);
+  padding: 100px;
+  color: ${theme.color.white};
+`;
--- packages/service/src/pages/PartsCollection/PartsCollection.tsx
+++ packages/service/src/pages/PartsCollection/PartsCollection.tsx
@@ -1,5 +1,11 @@
-import * as style from "./Main.css";
+import { CustomCard } from "@service/components/partsCollection";
+import * as style from "./PartsCollection.css";
 
 export const PartsCollection = () => {
-  return <></>;
+  return (
+    <div css={style.mainBg}>
+      <h1 css={style.pageTitle}>내 아반떼 N 파츠 컬렉션</h1>
+      <CustomCard />
+    </div>
+  );
 };
