775e15ce14f81159132e0857119daaad38d37d93
DaeWon9 <qkreodnjs97@naver.com>
2024-08-13 23:35:17 +0900

    chore: Admin 페이지 초기 구성

--- packages/admin/src/index.tsx
+++ packages/admin/src/index.tsx
@@ -1,11 +1,25 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
-import { App } from "./App";
+import { Global } from "@emotion/react";
+import { router } from "./router.tsx";
+import { RouterProvider } from "react-router-dom";
+import { ModalProvider } from "@watermelon-clap/core/src/providers";
+import { globalStyles } from "@watermelon-clap/core/src/theme";
+import { ModalContainer } from "./common/components/ModalContainer";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 0 },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <ModalProvider>
+      <Global styles={globalStyles} />
+      <RouterProvider router={router} />
+      <ModalContainer />
+    </ModalProvider>
+  </QueryClientProvider>,
 );
--- packages/admin/src/router.tsx
+++ packages/admin/src/router.tsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import { MainPage } from "./pages/MainPage";
+import { OrderEventManagement } from "./pages/orderEvent/OrderEventManagement/OrderEventManagement";
+import { App } from "./App";
+import { MAIN_PAGE_ROUTE } from "./constants/routes";
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <MainPage />,
+    path: "",
+    element: <App />,
+    children: [{ path: MAIN_PAGE_ROUTE, element: <OrderEventManagement /> }],
   },
 ]);
