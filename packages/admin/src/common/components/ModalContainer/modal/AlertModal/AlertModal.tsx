87fcb85831cc2f81d5c4c798f8e76895659d5a24
DaeWon9 <qkreodnjs97@naver.com>
2024-08-13 23:38:46 +0900

    feat: AlertModal 구현

---
+++ packages/admin/src/common/components/ModalContainer/modal/AlertModal/AlertModal.tsx
@@ -0,0 +1,36 @@
+import Modal from "react-modal";
+import { Button } from "../../../Button";
+import { useScrollStop } from "@watermelon-clap/core/src/hooks";
+import { alertModalStyles, alertModalBodyStyles } from "./AlertModal.css";
+import { ReactNode } from "react";
+import { DefaultModalProps } from "../../ModalContainer";
+
+export interface AlertModalProps extends DefaultModalProps {
+  title: ReactNode;
+  content: ReactNode;
+}
+
+export const AlertModal = ({
+  isOpen,
+  onRequestClose,
+  title,
+  content,
+}: AlertModalProps) => {
+  useScrollStop(isOpen);
+
+  return (
+    <Modal
+      shouldCloseOnEsc
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      style={alertModalStyles}
+      ariaHideApp={false}
+    >
+      <div css={alertModalBodyStyles}>
+        {title}
+        <div>{content}</div>
+        <Button onClick={onRequestClose}>확인</Button>
+      </div>
+    </Modal>
+  );
+};
