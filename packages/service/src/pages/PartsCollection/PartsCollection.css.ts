462b1a898b86b789d86cf51ab47cd9e7f60948dc
thgee <thgeee@naver.com>
2024-08-14 11:01:17 +0900

    feat: [PartsCollection 페이지] Title 모바일 반응형 적용

--- packages/service/src/pages/PartsCollection/PartsCollection.css.ts
+++ packages/service/src/pages/PartsCollection/PartsCollection.css.ts
@@ -1,4 +1,5 @@
 import { css } from "@emotion/react";
+import { mobile } from "@service/common/responsive/responsive";
 import { theme } from "@watermelon-clap/core";
 
 export const mainBg = css`
@@ -13,4 +14,9 @@ export const pageTitle = css`
   font-size : calc(50px + 2vw);
   padding: 100px;
   color: ${theme.color.white};
+
+  ${mobile(css`
+    font-size: calc(20px + 2vw);
+    padding: 100px 0 50px 0;
+  `)}
 `;
