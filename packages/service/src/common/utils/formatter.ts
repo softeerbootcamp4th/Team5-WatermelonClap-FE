97d14a1e8dcc415243015d4c9980075a71e00a5d
DaeWon9 <qkreodnjs97@naver.com>
2024-08-14 18:50:34 +0900

    feat: phoneNumberAutoFormat util 구현

--- packages/service/src/common/utils/formatter.ts
+++ packages/service/src/common/utils/formatter.ts
@@ -5,3 +5,13 @@ export function toPx(value: number | string): string {
 export function toS(value: number | string): string {
   return typeof value === "number" ? `${value}s` : value;
 }
+
+export const phoneNumberAutoFormat = (phoneNumber: string): string => {
+  const number = phoneNumber.trim().replace(/[^0-9]/g, "");
+
+  if (number.length < 4) return number;
+  if (number.length < 7) return number.replace(/(\d{3})(\d{1})/, "$1-$2");
+  if (number.length < 11)
+    return number.replace(/(\d{3})(\d{3})(\d{1})/, "$1-$2-$3");
+  return number.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3");
+};
