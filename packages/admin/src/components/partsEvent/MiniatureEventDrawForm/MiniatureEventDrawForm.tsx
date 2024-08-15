1835de35a8da79714efcc33e7f80e158aef811a4
DaeWon9 <qkreodnjs97@naver.com>
2024-08-16 00:09:27 +0900

    feat: MiniatureEventDrawFrom 컴포넌트 구현

---
+++ packages/admin/src/components/partsEvent/MiniatureEventDrawForm/MiniatureEventDrawForm.tsx
@@ -0,0 +1,13 @@
+import { RewardInfo } from "../RewardInfo";
+
+export const MiniatureEventDrawFrom = () => {
+  return (
+    <div>
+      <RewardInfo
+        rank={1}
+        winnerCount={10}
+        reward="커스텀된 아반떼 N 미니어처"
+      />
+    </div>
+  );
+};
