43c62d6c6fb5099e9811e75f4a0161c61ce4c458
thgee <thgeee@naver.com>
2024-08-15 11:41:54 +0900

    feat: 클립보드 복사 기능 구현

--- packages/service/src/common/components/ClipBoardButton/ClipBoardButton.tsx
+++ packages/service/src/common/components/ClipBoardButton/ClipBoardButton.tsx
@@ -8,51 +8,61 @@ import {
 } from "./ClipBoardButton.css";
 import { ReactComponent as ClipBoardIcon } from "public/icons/clipboard.svg";
 
-const ClipBoardButton = () => {
+interface IClipBoardButton {
+  copyRef: React.MutableRefObject<null>;
+}
+const ClipBoardButton = ({ copyRef }: IClipBoardButton) => {
   const [isCliped, setIsCliped] = useState<boolean>(false);
   const initialText = "클립보드에 복사";
   const changeText = "복사되었습니다!";
 
+  const handleCopy = () => {
+    const textToCopy = copyRef?.current?.textContent;
+    navigator.clipboard.writeText(textToCopy);
+  };
+
   return (
-    <AnimatePresence mode="wait">
-      {isCliped ? (
-        <motion.button
-          css={clipButtonSuccessStyle}
-          onClick={() => setIsCliped(false)}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <motion.span
-            css={clipButtonSuccessContentStyle}
-            key="action"
-            initial={{ y: -50 }}
-            animate={{ y: 0 }}
+    <div onClick={handleCopy}>
+      <AnimatePresence mode="wait">
+        {isCliped ? (
+          <motion.button
+            css={clipButtonSuccessStyle}
+            onClick={() => setIsCliped(false)}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
           >
-            <ClipBoardIcon />
-            {changeText}
-          </motion.span>
-        </motion.button>
-      ) : (
-        <motion.button
-          css={clipButtonStyle}
-          onClick={() => setIsCliped(true)}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <motion.span
-            css={clipButtonContentStyle}
-            key="reaction"
-            initial={{ x: 0 }}
-            exit={{ x: 50, transition: { duration: 0.1 } }}
+            <motion.span
+              css={clipButtonSuccessContentStyle}
+              key="action"
+              initial={{ y: -50 }}
+              animate={{ y: 0 }}
+            >
+              <ClipBoardIcon />
+              {changeText}
+            </motion.span>
+          </motion.button>
+        ) : (
+          <motion.button
+            css={clipButtonStyle}
+            onClick={() => setIsCliped(true)}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
           >
-            <ClipBoardIcon />
-            {initialText}
-          </motion.span>
-        </motion.button>
-      )}
-    </AnimatePresence>
+            <motion.span
+              css={clipButtonContentStyle}
+              key="reaction"
+              initial={{ x: 0 }}
+              exit={{ x: 50, transition: { duration: 0.1 } }}
+            >
+              <ClipBoardIcon />
+              {initialText}
+            </motion.span>
+          </motion.button>
+        )}
+      </AnimatePresence>
+    </div>
   );
 };
 
