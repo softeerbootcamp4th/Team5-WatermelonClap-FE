71b2000991022ee907f9adaca6f22de64180855b
DaeWon9 <qkreodnjs97@naver.com>
2024-08-15 23:13:51 +0900

    feat: 기대평 토글 기능 구현

--- packages/admin/src/components/partsEvent/ExpectationDataGrid/ExpectationDataGrid.tsx
+++ packages/admin/src/components/partsEvent/ExpectationDataGrid/ExpectationDataGrid.tsx
@@ -1,7 +1,10 @@
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { IExpectation } from "@admin/apis/expectation/type";
 import { useQuery } from "@tanstack/react-query";
-import { apiGetExpectation } from "@admin/apis/expectation/apiGetExpectation";
+import { apiGetExpectation } from "@admin/apis/expectation";
+import Checkbox from "@mui/material/Checkbox";
+import { ChangeEvent, useEffect, useState } from "react";
+import { apiPostExpectation } from "@admin/apis/expectation/apiPostExpectaion";
 
 function mapDataToGrid(data: IExpectation[] | undefined) {
   if (!Array.isArray(data)) {
@@ -19,19 +22,46 @@ function mapDataToGrid(data: IExpectation[] | undefined) {
 
 export const ExpectationDataGrid = () => {
   const { data: expectationList } = useQuery<IExpectation[]>({
-    queryKey: ["participantList"],
+    queryKey: ["expectationList"],
     queryFn: () => apiGetExpectation(),
-    staleTime: Infinity,
   });
 
-  const gridData = mapDataToGrid(expectationList);
+  const [gridData, setGridData] = useState(mapDataToGrid(expectationList));
+
+  useEffect(() => {
+    setGridData(mapDataToGrid(expectationList));
+  }, [expectationList]);
+
+  const handleCheckboxChange = (
+    id: string,
+    event: ChangeEvent<HTMLInputElement>,
+  ) => {
+    event.stopPropagation();
+    apiPostExpectation(id).then((res) => console.log(res));
+
+    setGridData((prevData) =>
+      prevData.map((row) =>
+        row.id === id ? { ...row, approved: event.target.checked } : row,
+      ),
+    );
+  };
 
   const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 200 },
+    { field: "id", headerName: "ID", width: 100 },
     { field: "uid", headerName: "user ID", width: 200 },
     { field: "time", headerName: "등록 시간", width: 200 },
-    { field: "content", headerName: "기대평", width: 400 },
-    { field: "approved", headerName: "게시 여부", width: 200 },
+    { field: "content", headerName: "기대평", width: 600 },
+    {
+      field: "approved",
+      headerName: "게시 여부",
+      width: 200,
+      renderCell: (params) => (
+        <Checkbox
+          checked={params.value}
+          onChange={(event) => handleCheckboxChange(params.row.id, event)}
+        />
+      ),
+    },
   ];
 
   return (
