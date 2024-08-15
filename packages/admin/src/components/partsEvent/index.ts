05874ad069f97d178d2b0c001db4d60a36723284
DaeWon9 <qkreodnjs97@naver.com>
2024-08-16 00:10:20 +0900

    chore: partsEvent 관련 컴포넌트 다시내보내기 구성

--- packages/admin/src/components/partsEvent/index.ts
+++ packages/admin/src/components/partsEvent/index.ts
@@ -1,1 +1,5 @@
 export * from "./ParticipantDataGrid";
+export * from "./ExpectationDataGrid";
+export * from "./RewardInfo";
+export * from "./MainEventDrawForm";
+export * from "./MiniatureEventDrawForm";
