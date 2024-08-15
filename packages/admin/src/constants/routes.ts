2f64622b42d3569844cd1c16248f1bfe095e874c
DaeWon9 <qkreodnjs97@naver.com>
2024-08-16 00:17:14 +0900

    chore: ORDER_EVENT_WINNER_MANAGEMENT_PAGE_ROUTE 경로 변경

--- packages/admin/src/constants/routes.ts
+++ packages/admin/src/constants/routes.ts
@@ -3,7 +3,7 @@ export const ORDER_EVENT_MANAGEMENT_PAGE_ROUTE =
 export const ORDER_EVENT_GENERATION_PAGE_ROUTE =
   "/order-event-generation" as const;
 export const ORDER_EVENT_WINNER_MANAGEMENT_PAGE_ROUTE =
-  "/order-event-winner-generation" as const;
+  "/order-event-winner-management" as const;
 
 export const PARTS_EVENT_GENERATION_PAGE_ROUTE =
   "/parts-event-generation" as const;
