de13269d12f289b921039eeccc61e943b7df9db3
DaeWon9 <qkreodnjs97@naver.com>
2024-08-19 14:55:05 +0900

    chore: 날짜 수정

--- packages/service/src/components/nQuizEvent/NQuizTitle/NQuizTitle.tsx
+++ packages/service/src/components/nQuizEvent/NQuizTitle/NQuizTitle.tsx
@@ -17,7 +17,7 @@ export const NQuizTitle = () => {
         <NLogo css={logoStyle} />
         <span css={titleStyle}>퀴즈</span>
       </div>
-      <div css={dateStyle}>2024. 07. 29 ~ 08. 02</div>
+      <div css={dateStyle}>2024. 08. 19 ~ 08. 23</div>
       <div css={contentContainerStyle}>
         <div css={contentTextStyle}>
           아반떼 N에 관한 정보를 알아보는 간단한 퀴즈를 맞혀보세요!
