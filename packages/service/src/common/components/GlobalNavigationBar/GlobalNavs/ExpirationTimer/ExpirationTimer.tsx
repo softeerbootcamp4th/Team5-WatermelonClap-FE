7df27071a51732f52409b952f74140792157e5f7
thgee <thgeee@naver.com>
2024-08-19 14:45:43 +0900

    feat: 토큰 만료시간 타이머 컴포넌트 구현

---
+++ packages/service/src/common/components/GlobalNavigationBar/GlobalNavs/ExpirationTimer/ExpirationTimer.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import * as style from "./ExpirationTimer.css";
+
+interface IExpirationTimerProps {
+  diffMs: number;
+}
+
+export const ExpirationTimer = ({ diffMs }: IExpirationTimerProps) => {
+  const [remainingTime, setRemainingTime] = useState(diffMs);
+  useEffect(() => {
+    if (remainingTime <= 0) return;
+
+    const timerId = setInterval(() => {
+      setRemainingTime((prevTime) => {
+        const newTime = prevTime - 1000;
+        if (newTime <= 0) {
+          clearInterval(timerId);
+          return 0;
+        }
+        return newTime;
+      });
+    }, 1000);
+
+    return () => clearInterval(timerId);
+  }, [remainingTime]);
+
+  if (remainingTime <= 0) {
+    return <span>Expired</span>;
+  }
+
+  const diffHrs = String(Math.floor(remainingTime / (1000 * 60 * 60))).padStart(
+    2,
+    "0",
+  );
+  const diffMins = String(
+    Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60)),
+  ).padStart(2, "0");
+  const diffSecs = String(
+    Math.floor((remainingTime % (1000 * 60)) / 1000),
+  ).padStart(2, "0");
+
+  return (
+    <span css={style.timer}>
+      {diffHrs} : {diffMins} : {diffSecs}
+    </span>
+  );
+};
