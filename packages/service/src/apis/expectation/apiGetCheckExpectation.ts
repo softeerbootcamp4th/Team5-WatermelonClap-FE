d556d8bf3c8ca5b7c85962bd70c6035d6f2f9c60
thgee <thgeee@naver.com>
2024-08-21 18:26:45 +0900

    chore: apiGetCheckExpectation에 accesstoken 추가

--- packages/service/src/apis/expectation/apiGetCheckExpectation.ts
+++ packages/service/src/apis/expectation/apiGetCheckExpectation.ts
@@ -1,8 +1,15 @@
-import { customFetch } from "@watermelon-clap/core/src/utils";
+import { customFetch, getAccessToken } from "@watermelon-clap/core/src/utils";
 import { IApiGetCheckExpectation } from "./type";
 
 export const apiGetCheckExpectation = (): Promise<IApiGetCheckExpectation> => {
-  return customFetch(`${import.meta.env.VITE_BACK_BASE_URL}/expectations/check`)
+  return customFetch(
+    `${import.meta.env.VITE_BACK_BASE_URL}/expectations/check`,
+    {
+      headers: {
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    },
+  )
     .then((res) => res.json())
     .catch((error) => {
       throw error;
