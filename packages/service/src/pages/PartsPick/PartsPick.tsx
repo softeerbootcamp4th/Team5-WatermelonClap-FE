c5bf875dec73eca34a985ab137376530fe5981c9
thgee <thgeee@naver.com>
2024-08-21 15:28:03 +0900

    chore: ClickInduceIcon 컴포넌트 분리

--- packages/service/src/pages/PartsPick/PartsPick.tsx
+++ packages/service/src/pages/PartsPick/PartsPick.tsx
@@ -17,11 +17,7 @@ import { apiGetLotteryStatus } from "@service/apis/lottery/apiGetLotteryStatus";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { getAccessToken } from "@watermelon-clap/core/src/utils";
 import { IParts } from "@watermelon-clap/core/src/types";
-import { motion } from "framer-motion";
-import { css } from "@emotion/react";
-import { PiMouseLeftClickFill } from "react-icons/pi";
-
-import { theme } from "@watermelon-clap/core/src/theme";
+import { ClickInduceIcon } from "@service/components/ClickInduceIcon";
 
 enum Category {
   REAR = "스포일러",
@@ -94,45 +90,9 @@ export const PartsPick = () => {
           partsInfo={partsInfo}
           setPartsInfo={setPartsInfo}
         />
-        <motion.div
-          css={css`
-            position: relative;
-            top: 60px;
-            z-index: 2;
-          `}
-          transition={{
-            duration: 0.8,
-            repeat: Infinity,
-            repeatType: "reverse",
-            ease: "easeInOut",
-          }}
-          animate={{
-            y: -100,
-          }}
-        >
-          <div
-            css={[
-              theme.flex.column,
-              css`
-                color: ${theme.color.gray100};
-                gap: 4px;
-                filter: drop-shadow(0px 0px 5px #fff);
-              `,
-            ]}
-          >
-            <PiMouseLeftClickFill size={40} />
-            <h4
-              css={[
-                theme.font.pcpB,
-                css`
-                  margin-top: 4px;
-                `,
-              ]}
-            >
-              CLICK
-            </h4>
-          </div>
-        </motion.div>
+
+        <ClickInduceIcon />
+
         <Space size={isMobile ? 4 : 6} />
         {isPickComplete && (
           <p css={partsNameStyle}>
