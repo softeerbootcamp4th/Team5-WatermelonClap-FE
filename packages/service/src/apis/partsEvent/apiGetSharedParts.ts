c294bcacf8572b621b2132616b9d3706aac3146c
thgee <thgeee@naver.com>
2024-08-21 20:18:03 +0900

    chore: apiGetSharedParts에서 쿠키 저장할 수 있도록 옵션 수정

--- packages/service/src/apis/partsEvent/apiGetSharedParts.ts
+++ packages/service/src/apis/partsEvent/apiGetSharedParts.ts
@@ -4,5 +4,6 @@ import { customFetch } from "@watermelon-clap/core/src/utils";
 export const apiGetSharedParts = (link_key?: string): Promise<IMyParts[]> => {
   return customFetch(
     `${import.meta.env.VITE_BACK_BASE_URL}/event/parts/link/${link_key}`,
+    { credentials: "include" },
   ).then((res) => res.json());
 };
