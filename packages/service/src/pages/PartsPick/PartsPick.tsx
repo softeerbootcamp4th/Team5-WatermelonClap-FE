b795b9e7a6a21d38cbef81128457b23eaefe30c9
thgee <thgeee@naver.com>
2024-08-21 15:19:21 +0900

    feat: 파츠뽑기 페이지 클릭 유도 UI 구현

--- packages/service/src/pages/PartsPick/PartsPick.tsx
+++ packages/service/src/pages/PartsPick/PartsPick.tsx
@@ -17,6 +17,11 @@ import { apiGetLotteryStatus } from "@service/apis/lottery/apiGetLotteryStatus";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { getAccessToken } from "@watermelon-clap/core/src/utils";
 import { IParts } from "@watermelon-clap/core/src/types";
+import { motion } from "framer-motion";
+import { css } from "@emotion/react";
+import { PiMouseLeftClickFill } from "react-icons/pi";
+
+import { theme } from "@watermelon-clap/core/src/theme";
 
 enum Category {
   REAR = "스포일러",
@@ -80,6 +85,7 @@ export const PartsPick = () => {
     <>
       <div css={partsPickBackgroundStyle}>
         <PickTitle />
+
         <PartsCard
           backImage="/images/parts/back.svg"
           isMouseOutAnimationEnabled={false}
@@ -88,6 +94,45 @@ export const PartsPick = () => {
           partsInfo={partsInfo}
           setPartsInfo={setPartsInfo}
         />
+        <motion.div
+          css={css`
+            position: relative;
+            top: 60px;
+            z-index: 2;
+          `}
+          transition={{
+            duration: 0.8,
+            repeat: Infinity,
+            repeatType: "reverse",
+            ease: "easeInOut",
+          }}
+          animate={{
+            y: -100,
+          }}
+        >
+          <div
+            css={[
+              theme.flex.column,
+              css`
+                color: ${theme.color.gray100};
+                gap: 4px;
+                filter: drop-shadow(0px 0px 5px #fff);
+              `,
+            ]}
+          >
+            <PiMouseLeftClickFill size={40} />
+            <h4
+              css={[
+                theme.font.pcpB,
+                css`
+                  margin-top: 4px;
+                `,
+              ]}
+            >
+              CLICK
+            </h4>
+          </div>
+        </motion.div>
         <Space size={isMobile ? 4 : 6} />
         {isPickComplete && (
           <p css={partsNameStyle}>
