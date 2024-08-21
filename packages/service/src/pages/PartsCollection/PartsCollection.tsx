aa2e7b020d1f9d68a12788893fdcd9891ff55416
DaeWon9 <qkreodnjs97@naver.com>
2024-08-21 16:22:33 +0900

    feat: CustomCard 외부에 PinContainer 적용

--- packages/service/src/pages/PartsCollection/PartsCollection.tsx
+++ packages/service/src/pages/PartsCollection/PartsCollection.tsx
@@ -1,30 +1,48 @@
 import { CustomCard } from "@service/components/partsCollection";
 import * as style from "./PartsCollection.css";
 import { PartsTab } from "@service/components/partsCollection/PartsTab";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { apiGetMyParts } from "@service/apis/partsEvent/apiGetMyParts";
 import { useEffect, useState } from "react";
 import { ICustomCardProps } from "@service/components/partsCollection/CustomCard/CustomCard";
+import { useAuth } from "@watermelon-clap/core/src/hooks";
 import { IMyParts } from "@watermelon-clap/core/src/types";
 import { getAccessToken } from "@watermelon-clap/core/src/utils";
+import { PinContainer } from "@service/components/partsCollection/PinContainer";
 
 export const PartsCollection = () => {
-  const [equippedPartsImg, setEquippedPartsImg] = useState<ICustomCardProps>();
+  const { getIsLogin, login, handleTokenError } = useAuth();
 
-  const { data: partsDatas, refetch } = useSuspenseQuery<IMyParts[]>({
+  const [equippedParts, setEquippedParts] = useState<ICustomCardProps>();
+
+  const getPartsData = () =>
+    apiGetMyParts().catch(handleTokenError) as Promise<IMyParts[]>;
+
+  const { data: partsDatas, refetch } = useQuery<IMyParts[]>({
     queryKey: ["myParts", getAccessToken()],
-    queryFn: apiGetMyParts,
+    queryFn: getPartsData,
   });
 
   useEffect(() => {
-    setEquippedPartsImg(getEquippedPartsImg(partsDatas));
+    getIsLogin() ||
+      login().then(() => {
+        refetch();
+      });
+  }, []);
+
+  useEffect(() => {
+    setEquippedParts(getEquippedPartsImg(partsDatas));
   }, [partsDatas]);
 
   return (
     <div css={style.mainBg}>
       <h1 css={style.pageTitle}>내 아반떼 N 파츠 컬렉션</h1>
       <div css={style.partsContainer}>
-        <CustomCard {...equippedPartsImg} />
+        <div css={style.customCardContainer}>
+          <PinContainer equippedParts={equippedParts}>
+            <CustomCard {...equippedParts} />
+          </PinContainer>
+        </div>
 
         <PartsTab partsDatas={partsDatas} refetchGetMyParts={refetch} />
       </div>
@@ -33,27 +51,27 @@ export const PartsCollection = () => {
 };
 
 const getEquippedPartsImg = (partsDatas?: IMyParts[]) => {
-  const _equippedPartsImg: ICustomCardProps = {};
+  const _equippedParts: ICustomCardProps = {};
 
   partsDatas?.map((cate) =>
     cate.parts.map((parts) => {
       if (parts.equipped) {
         switch (parts.category) {
           case "DRIVE_MODE":
-            _equippedPartsImg.bgImg = parts.imgSrc;
+            _equippedParts.bgParts = parts;
             break;
           case "COLOR":
-            _equippedPartsImg.colorImg = parts.imgSrc;
+            _equippedParts.colorParts = parts;
             break;
           case "REAR":
-            _equippedPartsImg.spoilerImg = parts.imgSrc;
+            _equippedParts.spoilerParts = parts;
             break;
           case "WHEEL":
-            _equippedPartsImg.wheelImg = parts.imgSrc;
+            _equippedParts.wheelParts = parts;
             break;
         }
       }
     }),
   );
-  return _equippedPartsImg;
+  return _equippedParts;
 };
