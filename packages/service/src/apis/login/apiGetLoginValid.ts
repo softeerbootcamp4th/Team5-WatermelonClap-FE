f7ae03d4af2bb6ac05a170ed56583b4ad81636b8
thgee <thgeee@naver.com>
2024-08-22 14:56:00 +0900

    feat:  apiGetLoginValid 생성

---
+++ packages/service/src/apis/login/apiGetLoginValid.ts
@@ -0,0 +1,8 @@
+import { customFetch, getAccessToken } from "@watermelon-clap/core/src/utils";
+
+export const apiGetLoginValid = () =>
+  customFetch(`${import.meta.env.VITE_BACK_BASE_URL}/event/lotteries/login`, {
+    headers: {
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  });
