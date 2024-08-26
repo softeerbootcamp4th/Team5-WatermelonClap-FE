9d9f15bd6935b53f3df6df1f6c3c6e0e7a35477d
DaeWon9 <qkreodnjs97@naver.com>
2024-08-26 12:01:29 +0900

    refactor: useModal 훅에도 제네릭 타입 적용

--- packages/core/src/hooks/useModal.ts
+++ packages/core/src/hooks/useModal.ts
@@ -1,19 +1,19 @@
 import { useContext } from "react";
 import { ModalSetterContext, ModalState } from "../contexts/ModalContext";
 
-export const useModal = () => {
+export const useModal = <T = unknown, P = unknown>() => {
   const setModalState = useContext(ModalSetterContext);
 
   if (!setModalState) {
     throw new Error("useModal must be used within a ModalProvider");
   }
 
-  const openModal = ({ type, props = null }: ModalState) => {
-    setModalState({ type, props });
+  const openModal = (modalState: ModalState<T, P>) => {
+    setModalState(modalState);
   };
 
   const closeModal = () => {
-    setModalState({ type: null, props: null });
+    setModalState({ type: null, props: null } as ModalState<T, P>);
   };
 
   return { openModal, closeModal };
